0a4cdcfdaa5f8d826f9419f525cc755b6d06b57b
cubee <lijing0033@souhu.com>
2010-08-12 21:42:01 +0800

    iiiiii

--- test/src/kissy/kissy.js
+++ test/src/kissy/kissy.js
@@ -113,6 +113,7 @@
 			//when a module is add to KISSY via S.add() after "domReady" event,
 			//its callback function should be exec immediately
 			if(!(isReady && !afterReady)){
+				console.log('============'+name + isReady + ' '+ afterReady);
 				fn(self);
 			}
 
