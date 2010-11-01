a968bbb8a1545f1b0149263b2845fb3fd2bf60c0
bachi <bachi@Taobao-uedbj.taobao.ali.com>
2010-11-01 15:05:49 +0800

    kissy nodejs

---
+++ gallery/node/a.js
@@ -0,0 +1,4 @@
+KISSY.add('a',function(S){
+	S.a = 1;	
+});
+console.log('a.js run');
--- gallery/node/kissy-server.js
+++ gallery/node/kissy-server.js
@@ -1,86 +1,51 @@
-#!/usr/bin/env node
-var sys = require('sys'),
-    url = require('url'),
-    http = require('http');
 
-var YUI = require("yui3").YUI;
+require('./kissy');
 
-var jsdom  = require("jsdom"),
-    window = jsdom.jsdom().createWindow();
-
-
-
-
-
-require("assert").equal( global.YUI, undefined, "global yui created");
-
-
-YUI({
-    filter: 'debug',
-    debug: true,
-	modules:{
-		'ks-core':{
-			fullpath:'http://kissyteam.github.com/kissy/src/kissy/kissy.js',
-			requires:['skin','node']
-		},
-		slide:{
-			fullpath:'http://kissyteam.github.com/kissy/src/switchable/switchable.js',
-			requires:['ks-core','skin']
-		},
-		skin:{
-			fullpath:'http://kissyteam.github.com/kissy/build/cssbase/base-min.css',
-			type:'css'
-		}
-	}
-}).use('nodejs-dom','ks-core', function(Y) {
-    document = Y.Browser.document;
-	window = Y.Browser.window;
-	location = Y.Browser.location;
-	console.log(KISSY);
-	return;
-	Y.config.doc.body.innerHTML = ['<style>',
-		'#demo1 { position: relative; width: 750px; padding-top: 29px; }',
-		'#demo1 .ks-switchable-nav { position: absolute; left: 20px; margin-top: -29px; z-index: 99; }',
-		'#demo1 .ks-switchable-nav li {',
-		'		float: left;',
-		'			width: 130px;',
-		'			height: 27px;',
-		'			line-height: 21px;',
-		'			text-align: center;',
-		'			background: url(assets/tabs-sprite.gif) no-repeat 0 6px;',
-		'			margin-right: 3px;',
-		'			padding-top: 8px;',
-		'			cursor: pointer;',
-		'		}',
-		'		#demo1 .ks-switchable-nav li.ks-active { background-position: 0 -40px; cursor: default; }',
-		'		#demo1 .ks-switchable-content {',
-		'			position: relative;',
-		'			height: 120px;',
-		'			padding: 20px;',
-		'			border: 1px solid #AEC7E5;',
-		'		}',
-		'		</style><div id="demo1"></div>'].join('');
-	var docType = '<!DOCTYPE html>';
-
-	http.createServer(function (req, res) {
-
-		new KISSY.Tabs('#demo1');
-
-
-
-		res.writeHead(200, {
-			'Content-Type': 'text/html',
-			'Accept-Charset':'utf-8'}
-		);
-		var out = docType + Y.one('doc').get('outerHTML');
-		res.write(out);
-		res.close();
-
-
-
-	}).listen(81);
-
-		
-
-
+var S = KISSY;
+/*
+//it work
+S.add('abc',function(S){
+	S.a = '===============================';	
 });
+S.use('abc',function(S){
+	S.log(S.a);
+});
+*/
+
+
+/*
+//it work
+S.add('a', {
+    fullpath: 'a.js'
+}).use('a', function(S) {
+    S.log(S.a);
+});
+*/
+
+/*
+//it work
+S.add('a', {
+    fullpath: 'http://taobao-wd.ns1.name/bachi/jayli/jayli.github.com/gallery/node/a.js'
+}).use('a', function(S) {
+    S.log(S.a);
+});
+*/
+
+
+/*
+//程序不工作，因为这里没有domReady
+*/
+/*
+S.add('a', {
+    fullpath: 'a.js'
+}).ready(function(S) {
+    S.log(S.a);
+});
+*/
+
+S.use('ks-core', function(S) {
+    S.log(S);
+});
+
+//require("assert").equal( global.YUI, undefined, "global yui created");
+
