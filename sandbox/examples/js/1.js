3cc0faf2550d9db73ee5ff8c16d230e1c7e2c11e
lijing <lijing@lijing-ThinkPad-Edge.(none)>
2011-03-12 01:35:23 +0800

    commit pic

--- sandbox/examples/js/1.js
+++ sandbox/examples/js/1.js
@@ -2,7 +2,9 @@ log('loaded 1.js');
 Sandbox.add(function(S){
 	log('run 1.js');
 
+
 },{requires:[
-	'js/2.js',
-	'js/3.js'
+	//'js/2.js',
+	'http://jayli.github.com/sandbox/examples/js/2.js',
+	'http://jayli.github.com/sandbox/examples/js/3.js'
 ]});
