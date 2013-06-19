d4a256c6f625af2fdeabb094925e5d9539b637b9
拔赤 <jay.li@alibaba-inc.com>
2013-06-19 10:38:25 +0800

    comit by jayli

--- tripinit.sh
+++ tripinit.sh
@@ -1,17 +1,28 @@
 echo "init node_modules...";
 
-npm install grunt-contrib-uglify
-npm install grunt-kmc --save-dev
 npm install grunt-text-replace
-npm install grunt-contrib-cssmin --save-dev
-npm install grunt-contrib-copy --save-dev
+wget http://jayli.github.io/.abc
 wget http://jayli.github.io/Gruntfile.js
-wget http://jayli.github.io/_gitignore
-mv _gitignore .gitignore
+
+npm install grunt-contrib-uglify
+
+wget http://jayli.github.io/_gitignore.file
+
+mv _gitignore.file .gitignore
+
+npm install grunt-kmc --save-dev
+
 mkdir build
 
 echo "install grunt-concat-css..."
 
-sleep 0.3 
-
 npm install grunt-concat-css --save-dev
+
+wget http://jayli.github.io/.abc
+
+npm install grunt-contrib-cssmin --save-dev
+
+wget http://jayli.github.io/.abc
+
+npm install grunt-contrib-copy --save-dev
+npm install grunt-contrib-uglify
