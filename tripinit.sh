548d9f0e9228b3c9d56b6c8bf0944ea353ce9d4b
拔赤 <jay.li@alibaba-inc.com>
2013-06-19 10:04:43 +0800

    commit by jayli

--- tripinit.sh
+++ tripinit.sh
@@ -8,4 +8,5 @@ npm install grunt-text-replace
 npm install grunt-contrib-cssmin --save-dev
 npm install grunt-contrib-copy --save-dev
 wget http://jayli.github.io/Gruntfile.js
-wget http://jayli.github.io/.gitignore
+wget http://jayli.github.io/_gitignore
+mv _gitignore .gitignore
