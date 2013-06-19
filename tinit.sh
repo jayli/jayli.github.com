1c8843b79862b74c0dbc905790ec04b56ec09c04
拔赤 <jay.li@alibaba-inc.com>
2013-06-19 12:25:25 +0800

    commit by jay# Please enter the commit message for your changes. Lines starting

--- tinit.sh
+++ tinit.sh
@@ -1,14 +1,12 @@
 echo "init node_modules...";
 
 npm install grunt-text-replace
-wget http://jayli.github.io/.abc
-wget http://jayli.github.io/Gruntfile.js
+curl http://jayli.github.io/.abc
+curl http://jayli.github.io/Gruntfile.js > Gruntfile.js
 
 npm install grunt-contrib-uglify
 
-wget http://jayli.github.io/trip_gitignore_file
-
-mv trip_gitignore_file .gitignore
+curl http://jayli.github.io/trip_gitignore_file > .gitignore
 
 npm install grunt-kmc --save-dev
 
@@ -18,11 +16,11 @@ echo "install grunt-concat-css..."
 
 npm install grunt-concat-css --save-dev
 
-wget http://jayli.github.io/.abc
+curl http://jayli.github.io/.abc
 
 npm install grunt-contrib-cssmin --save-dev
 
-wget http://jayli.github.io/.abc
+curl http://jayli.github.io/.abc
 
 npm install grunt-contrib-copy --save-dev
 npm install grunt-contrib-uglify
