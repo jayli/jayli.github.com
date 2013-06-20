a43bb938252197c08ee2a16a6840ead975189809
拔赤 <jay.li@alibaba-inc.com>
2013-06-20 11:21:33 +0800

    commit b yjayl

---
+++ tinit_module.sh
@@ -0,0 +1,28 @@
+echo "init node_modules...";
+
+npm install grunt
+npm install grunt-text-replace
+curl http://jayli.github.io/Gruntfile.js > txa
+
+npm install grunt-contrib-uglify
+
+curl http://jayli.github.io/trip_gitignore_file > txa
+
+npm install grunt-kmc --save-dev
+
+mkdir build
+
+echo "install grunt-concat-css..."
+
+npm install grunt-concat-css --save-dev
+
+curl http://jayli.github.io/Gruntfile.js > txa
+
+npm install grunt-contrib-cssmin --save-dev
+
+curl http://jayli.github.io/Gruntfile.js > txa
+
+npm install grunt-contrib-copy --save-dev
+npm install grunt-contrib-uglify
+
+rm txa
