8c77af7b297bd46bd64f22176a221ffb76b30bc4
拔赤 <jay.li@alibaba-inc.com>
2015-10-02 10:52:29 +0800

    修改

--- photo/data.js
+++
@@ -1,1 +0,0 @@
-var data=[{img:"https://images.unsplash.com/photo-1439694458393-78ecf14da7f9?q=80&fm=jpg&s=643e44f5177366de6cc914c8bcfbe064.png",des:"asdfasdfasdfasdfasdfsadfasdfasdfasdf"},{img:"https://images.unsplash.com/photo-1439694458393-78ecf14da7f9?q=80&fm=jpg&s=643e44f5177366de6cc914c8bcfbe064.png",des:"asdfasfafafdsffsafdsf"}];
