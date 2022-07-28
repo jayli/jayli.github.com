a8be0144ec9f48ad63240e32b8b255a9d9c9e87a
jayli <bachi@taobao.com>
2022-07-28 18:47:19 +0800

    modifi local files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,90 @@
 module.exports = function(grunt) {
-    var task = grunt.task;
-        grunt.initConfig({
+  var task = grunt.task;
+  grunt.initConfig({
 
-                // 对build目录进行清理
-                clean: {
-                        build: {
-                                src: ['./index.html','./photo/assets/data.js']
+    // 对build目录进行清理
+    clean: {
+      build: {
+        src: ['./index.html','./photo/assets/data.js']
+      }
+    },
+    markdown: {
+      all: {
+        files: [
+          {
+            expand: true,
+            src: ['index.md','gallery/calendar/README.markdown'],
+            dest: './',
+            ext: '.html'
+          }
+        ]
+      },
+      options: {
+        template: 'markdown-template.jst',
+      }
+    },
+    replace: {
+      example: {
+        src: ['photo/data.md'],
+        dest: 'photo/assets/data.js',
+        replacements: [{
+          from: /^(http.+(png|jpeg|jpg|gif))$/igm,            // regex replacement ('Fooo' to 'Mooo')
+          to: "{img:'$1',"
+        }, {
+          from: /^(.+)$/igm,          // regex replacement ('Fooo' to 'Mooo')
+          to: function (matchedWord, index, fullText, regexMatches) {
+            if(/(png|jpeg|jpg|gif)$/.test(matchedWord)) {
+              return matchedWord;
+            } else if (/^\{/.test(matchedWord)) {
+              return matchedWord;
+            } else if (/^--.+--$/ig.test(matchedWord)) {
+              return '';
+            } else {
+              return "des:'" + matchedWord + "'},";
             }
-                },
-        markdown: {
-            all: {
-                files: [
-                    {
-                        expand: true,
-                        src: ['index.md','gallery/calendar/README.markdown'],
-                        dest: './',
-                        ext: '.html'
-                    }
-                ]
-            },
-            options: {
-                template: 'markdown-template.jst',
-            }
-        },
-        replace: {
-            example: {
-                src: ['photo/data.md'],
-                dest: 'photo/assets/data.js',
-                replacements: [{
-                    from: /^(http.+(png|jpeg|jpg|gif))$/igm,            // regex replacement ('Fooo' to 'Mooo')
-                    to: "{img:'$1',"
-                }, {
-                    from: /^(.+)$/igm,          // regex replacement ('Fooo' to 'Mooo')
-                    to: function (matchedWord, index, fullText, regexMatches) {
-                        if(/(png|jpeg|jpg|gif)$/.test(matchedWord)) {
-                            return matchedWord;
-                        } else if (/^\{/.test(matchedWord)) {
-                            return matchedWord;
-                        } else if (/^--.+--$/ig.test(matchedWord)) {
-                            return '';
-                        } else {
-                            return "des:'" + matchedWord + "'},";
-                        }
-                    }
-                }]
-            }
-        },
-        concat: {
-            options:{
-                stripBanners: true,
-                banner:"var PhotoList = [",
-                footer:"];",
-            },
-            dist: {
-                src: ['photo/assets/data.js'],
-                dest: 'photo/assets/data.js',
-            }
-        },
-        uglify:{
-            options: {
-                beautify: {
-                    ascii_only: true
-                }
-            },
-            main: {
-                files: [
-                    {
-                        expand: true,
-                        src: ['photo/assets/data.js'],
-                        dist: 'photo/assets/data.js'
-                    }
-                ]
-            },
+          }
+        }]
+      }
+    },
+    concat: {
+      options:{
+        stripBanners: true,
+        banner:"var PhotoList = [",
+        footer:"];",
+      },
+      dist: {
+        src: ['photo/assets/data.js'],
+        dest: 'photo/assets/data.js',
+      }
+    },
+    uglify:{
+      options: {
+        beautify: {
+          ascii_only: true
         }
+      },
+      main: {
+        files: [
+          {
+            expand: true,
+            src: ['photo/assets/data.js'],
+            dist: 'photo/assets/data.js'
+          }
+        ]
+      },
+    }
 
-        });
+  });
 
-        // 使用到的任务，可以增加其他任务
-        grunt.loadNpmTasks('grunt-contrib-clean');
-        grunt.loadNpmTasks('grunt-markdown');
-        grunt.loadNpmTasks('grunt-text-replace');
-        grunt.loadNpmTasks('grunt-contrib-concat');
-        grunt.loadNpmTasks('grunt-contrib-uglify');
+  // 使用到的任务，可以增加其他任务
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-markdown');
+  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-        return grunt.registerTask('default', '',function(type){
-        task.run(['clean','markdown','replace:example','concat','uglify']);
-    });
+  return grunt.registerTask('default', '',function(type){
+    task.run(['clean','markdown','replace:example','concat','uglify']);
+  });
 };
 
--- index.md
+++ index.md
@@ -3,7 +3,7 @@
 
 ### ➭ 本站简介
 
-拔赤，2008 年加入中国雅虎（[Yahoo 已死](http://jayli.github.io/blog/data/2010/12/16/yahoo.html)），后转战淘宝至今，一直做前端开发，目前在[阿里旅行·去啊](http://www.alitrip.com)前端团队，本站涉及 HTML/CSS/JavaScript 等技术观点，收录个人一些吐槽和技术作品，喜欢折腾，偶尔写写文章、[拍点照片儿](http://jayli.github.io/photo/)、翻译几本小书...
+拔赤，2008 年加入中国雅虎（[Yahoo 已死](http://jayli.github.io/blog/data/2010/12/16/yahoo.html)），后转淘宝至今，一直做前端开发，目前在[阿里旅行·去啊](http://www.alitrip.com)前端团队，本站涉及 HTML/CSS/JavaScript 等技术观点，喜欢折腾，偶尔写写文章、[拍点照片](http://jayli.github.io/photo/)...
 
 您可以在[微博](http://www.weibo.com/jayli)或[微信](https://gw.alicdn.com/tps/TB14htbIXXXXXb4XpXXXXXXXXXX.png)上[at]到我
 
@@ -11,53 +11,36 @@
 
 ### ➭ 文章
 
-- 【译】[什么是 Node](http://jayli.github.io/whatisnode/)
-- 【译】[Nicholas C. Zakas vs John Resig 一场关于YUI3/jQuery的精彩辩论](http://ued.taobao.org/blog/2010/11/yui3-vs-jquery/)
+- [Nicholas C. Zakas vs John Resig 一场关于YUI3/jQuery的精彩辩论](http://ued.taobao.org/blog/2010/11/yui3-vs-jquery/)
 - [前端技术的开放和封闭](https://github.com/jayli/jayli.github.com/issues/18)
 - [爱梦想，去旅行，阿里旅行·去啊 H5 团队招募ing](https://github.com/jayli/jayli.github.com/issues/17)
 - [微不足道的坚持](https://github.com/jayli/jayli.github.com/issues/12)
 - [蒙台梭利的三段话](https://github.com/jayli/jayli.github.com/issues/14)
-- [前端开发十日谈](https://github.com/jayli/jayli.github.com/issues/1)
 - [2012 年前端技术回顾](http://www.csdn.net/article/1970-01-01/2815164)
 - [KISSY 的自成体系是一种趋势](http://www.infoq.com/cn/news/2013/07/bachi-on-kissy?utm_source=tuicool)
-- [好的组件无需太复杂 - KISSY Slide 组件简介](http://ued.taobao.org/blog/2013/06/kissy-slide/)
-- [JavaScript语法检查插件 jsLint for Vim](http://ued.taobao.org/blogobao.org/blog/2010/11/jslint-for-vim/)
-- [将你的 KISSY 程序移植到服务器端 – nodejs-kissy 项目简介](http://ued.taobao.org/blog/2010/11/nodejs-kissy/)
 - [YUI3 设计中的激进和妥协](http://ued.taobao.org/blog/2010/01/yui3%E8%AE%BE%E8%AE%A1%E4%B8%AD%E7%9A%84%E6%BF%80%E8%BF%9B%E5%92%8C%E5%A6%A5%E5%8D%8F/)
 
 ### ➭ 项目
 
-- [Vim-EasyDebugger](https://github.com/jayli/vim-easydebugger)（VIM）
 - [Vim-EasyComplete](https://github.com/jayli/vim-easycomplete)（VIM）
-- [我的 vim 配置](https://github.com/jayli/vim)（VIM）
 - [Combo Script](https://github.com/jayli/combo)（PHP）
 - [YUI3 Calendar](http://jayli.github.io/gallery/calendar/README.html)（JavaScript）
 - [基于 DOM 的碰撞检测 DEMO](http://jayli.github.io/gallery/canvas/g.html)（JavaScript）
 - 编码小工具：[utf8 URL 编码](http://jayli.github.io/gallery/encode/utf8urlencode.html)、[gbk URL 编码](http://www.taobao.com/market/trip/encode-test.php)、[Unicode 编码](http://jayli.github.io/gallery/encode/unicodeEncode.html)（JavaScript）
 - [SandBox](https://github.com/jayli/sandbox/)（JavaScript）
 - [YUI Slide](http://jayli.github.io/gallery/yuislide/)、[KISSY Slide](http://kissygalleryteam.github.io/slide/1.2/demo/index.html)（[1.3](http://kissygalleryteam.github.io/slide/1.3/demo/index.html)）（JavaScript）
-- [Tip](http://kissygalleryteam.github.io/tip/1.0/demo/index.html)（JavaScript）
 - [Kissy Mini](http://m.kissyui.com)（JavaScript）
-
-### ➭ 技术专利
-
-- [一种项目开发中数据包的加载方法及系统](http://www.soopat.com/Patent/201210009029)（2012）
-- [一种Web前端本地开发环境的搭建方法和装置](http://www.soopat.com/Patent/201110336122)（2011）
 
 ### ➭ 演讲 & 分享
 
 - QCon：[极致的 Hybrid](http://pan.baidu.com/s/1eQk0AlG)（2015）
 - QCon：[淘宝 HTML5 实战](http://wenku.baidu.com/view/a1653a13cc7931b765ce151b.html)（2011）
 - D2：[低碳的无线端开发实战](http://wenku.baidu.com/view/fa82b843e45c3b3567ec8bf3.html)（2012）
-- D2：[KISSY Mobile App Toolkit](http://wenku.baidu.com/view/461d4054240c844768eaee05)（2013）
-- 象声汇：[懂一点航旅前端技术（培训课）](http://vdisk.weibo.com/s/sWYf0np7mi6/1439391012)（2015）
 - 象声汇：[正念前端](http://wenku.baidu.com/view/484409440508763230121278)（2015）
 - 象声汇：[加速成长的六个要诀](http://wenku.baidu.com/view/a92c3f8b856a561252d36f6b.html)（2014）
-- 象声汇：[CSS 选择器性能调研](http://pan.baidu.com/s/1sjHUizN)（2013）
 - 象声汇：[JavaScript AutoLoad](http://wenku.baidu.com/view/47d44fcdc850ad02de8041f9)（2011）
 - 象声汇：[NodeJS 实战](http://wenku.baidu.com/view/6f8335c34028915f804dc2be.html)（2010）
 - 象声汇：[GUI 进化史](http://wenku.baidu.com/view/f319d68d6529647d2728521c.html)（2010）
-- 象声汇：[JavaScript 图形编程](http://wenku.baidu.com/view/d259189e227916888586d70d)（2010）
 
 ### ➭ 翻译
 
